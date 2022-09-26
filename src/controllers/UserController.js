05ee84b0de19974d65118bef57799f00a20b7d2c
Douglas Fernandes Pereira Junior <douglas.fernandes91@live.com>
2022-09-26 17:13:40 -0300

    add try catch & add req.body to UserController

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -3,11 +3,7 @@ import User from '../models/User';
 class UserController {
   async store(req, res) {
     try {
-      const novoUser = await User.create({
-        nome: 'Douglas',
-        email: 'douglas.fernandes91@live.com',
-        password: '123456',
-      });
+      const novoUser = await User.create(req.body);
       res.json(novoUser);
     } catch (e) {
       res.status(400).json({
