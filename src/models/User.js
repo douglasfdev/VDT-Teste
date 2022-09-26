7135b61e44f5b4fb811d3ca7e5f74796f50f3a26
Douglas Fernandes Pereira Junior <douglas.fernandes91@live.com>
2022-09-26 15:55:06 -0300

    add validator to sequelize on necessary fields

---
+++ src/models/User.js
@@ -0,0 +1,45 @@
+import Sequelize, { Model } from 'sequelize';
+
+export default class User extends Model {
+  static init(sequelize) {
+    super.init({
+      nome: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          len: {
+            args: [3, 255],
+            msg: 'O nome deve ter entre 3 e 255 caracteres',
+          },
+        },
+      },
+      email: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          isEmail: {
+            args: [3, 255],
+            msg: 'Email inválido',
+          },
+        },
+      },
+      password_hash: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+      },
+      password: {
+        type: Sequelize.VIRTUAL,
+        defaultValue: '',
+        validate: {
+          len: {
+            args: [6, 50],
+            msg: 'A senha deve ter entre 3 e 50 caracteres',
+          },
+        },
+      },
+    }, {
+      sequelize,
+    });
+    return this;
+  }
+}
