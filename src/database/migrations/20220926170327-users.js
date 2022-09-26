c90263e39bbc73c66eac44034e1507f1469e920c
Douglas Fernandes Pereira Junior <douglas.fernandes91@live.com>
2022-09-26 15:45:55 -0300

    create table user

--- src/database/migrations/20220926170327-users.js
+++ src/database/migrations/20220926170327-users.js
@@ -1,36 +1,40 @@
 module.exports = {
-  up: (queryInterface, Sequelize) => queryInterface.createTable(
-    'users',
-    {
-      id: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable(
+      'users',
+      {
+        id: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+        },
+        nome: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        email: {
+          type: Sequelize.STRING,
+          allowNull: false,
+          unique: true,
+        },
+        password_hash: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        created_at: {
+          type: Sequelize.DATE,
+          allowNull: false,
+        },
+        updated_at: {
+          type: Sequelize.DATE,
+          allowNull: false,
+        },
       },
-      nome: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      email: {
-        type: Sequelize.STRING,
-        allowNull: false,
-        unique: true,
-      },
-      password_hash: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      created_at: {
-        type: Sequelize.DATE,
-        allowNull: false,
-      },
-      updated_at: {
-        type: Sequelize.DATE,
-        allowNull: false,
-      },
-    },
-  ),
+    );
+  },
 
-  down: (queryInterface) => queryInterface.dropTable('users'),
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('users');
+  },
 };
