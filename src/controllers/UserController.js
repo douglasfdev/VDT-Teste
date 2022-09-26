a7a3f95d9e6e41d89ec0aefde3aa873ecb57e6cf
Douglas Fernandes Pereira Junior <douglas.fernandes91@live.com>
2022-09-26 18:30:37 -0300

    add rout & delete method user

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -48,11 +48,38 @@ class UserController {
       const novosDados = await user.update(req.body);
       return res.json(novosDados);
     } catch (e) {
-      return res.json(null);
+      return res.status(400).json({
+        erros: e.errors.map((err) => err.message),
+      });
     }
   }
 
   // Delete
+  async delete(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.status(400).json({
+          erros: ['ID Não enviado'],
+        });
+      }
+
+      const user = await User.findByPk(req.params.id);
+      if (!user) {
+        return res.status(400).json({
+          erros: ['Usuário não existe'],
+        });
+      }
+
+      await user.destroy();
+      return res.json({
+        msg: 'Usuário apagado com sucesso!',
+      });
+    } catch (e) {
+      return res.status(400).json({
+        erros: e.errors.map((err) => err.message),
+      });
+    }
+  }
 }
 
 export default new UserController();
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -7,5 +7,6 @@ router.post('/', userController.store);
 router.get('/', userController.index);
 router.get('/:id', userController.show);
 router.put('/:id', userController.update);
+router.delete('/:id', userController.delete);
 
 export default router;
