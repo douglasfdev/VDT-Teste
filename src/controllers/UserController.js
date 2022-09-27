f249cd0e2b95cefb2d8e1af9c2d5c831377a1fe7
Douglas Fernandes Pereira Junior <douglas.fernandes91@live.com>
2022-09-26 23:16:53 -0300

    Chaging return of users & userID

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -15,6 +15,7 @@ class UserController {
   async index(req, res) {
     try {
       const users = await User.findAll({ attributes: ['id', 'nome', 'email'] });
+
       return res.json(users);
     } catch (e) {
       return res.json(null);
@@ -24,7 +25,9 @@ class UserController {
   async show(req, res) {
     try {
       const user = await User.findByPk(req.params.id);
-      return res.json(user);
+
+      const { id, nome, email } = user;
+      return res.json({ id, nome, email });
     } catch (e) {
       return res.json(null);
     }
@@ -32,13 +35,8 @@ class UserController {
 
   async update(req, res) {
     try {
-      if (!req.params.id) {
-        return res.status(400).json({
-          erros: ['ID Não enviado'],
-        });
-      }
+      const user = await User.findByPk(req.userId);
 
-      const user = await User.findByPk(req.params.id);
       if (!user) {
         return res.status(400).json({
           erros: ['Usuário não existe'],
