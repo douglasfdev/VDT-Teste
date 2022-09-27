708ceaf63e3df09ffffef3ad45e3f2e771cc4896
Douglas Fernandes Pereira Junior <douglas.fernandes91@live.com>
2022-09-26 23:35:54 -0300

    add return specif details in acc of users

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -4,7 +4,8 @@ class UserController {
   async store(req, res) {
     try {
       const novoUser = await User.create(req.body);
-      return res.json(novoUser);
+      const { id, nome, email } = novoUser;
+      return res.json({ id, nome, email });
     } catch (e) {
       return res.status(400).json({
         erros: e.errors.map((err) => err.message),
